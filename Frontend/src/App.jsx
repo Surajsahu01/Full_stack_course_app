b29d9491f75833a73bb3e84d08c96b2dc7041c9d
Suraj sahu <84985473+Surajsahu01@users.noreply.github.com>
2025-02-27 23:35:30 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 import Login from "./components/Login";
 import Signup from "./components/Signup";
